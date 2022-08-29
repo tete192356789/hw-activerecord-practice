e097a10d1e90cfab65310d2c49314de372bcbba7
tete192356789 <nachanon1923@hotmail.com>
2022-08-29 16:38:21 +0700

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.6.6'
+ruby '3.0.3'
 
 gem 'activerecord', '~> 6'
 gem 'byebug'
--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -19,9 +19,59 @@ class Customer < ActiveRecord::Base
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+
+    Customer.where("first = 'Candice'")
   end
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE '%@%'")
+  end
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE '%.org'")
+  end
+
+  def self.with_invalid_email
+    Customer.where("email NOT LIKE '%@%'")
+  end
+
+  def self.with_blank_email
+    Customer.where("email IS NULL")
+  end
+
+  def self.born_before_1980
+    Customer.where("birthdate < ('1980-01-01')")
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email LIKE ? AND birthdate < ?","%@%",('1980-01-01'))
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE 'b%'").order("birthdate")
+  end
+
+  def self.twenty_youngest
+    Customer.order("birthdate DESC").limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    get_row = Customer.find_by(first: 'Gussie')
+    get_row.birthdate = "2004-02-08"
+    get_row.save
+    
+  end
+  
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email NOT LIKE '%@%'").update_all(email: nil)
+  end
+
+  def self.delete_meggie_herman
+    Customer.find_by(first: 'Meggie' , last: 'Herman').destroy
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where("birthdate < ('1977-12-31')").destroy_all
   end
   # etc. - see README.md for more details
 end
